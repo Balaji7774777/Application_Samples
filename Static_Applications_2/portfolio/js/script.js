217f208e4d9381aa38fade3c935b1e1bb0a5f9dd
Balaji Ramakrishnan <balajiramakrishnan74@gmail.com>
2026-07-04 17:51:10 +0530

    a

--- Static_Applications_2/portfolio/js/script.js
+++
@@ -1,8 +0,0 @@
-import "./navbar.js";
-import "./cursor.js";
-import "./particles.js";
-import "./animations.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Portfolio initialized");
-});
